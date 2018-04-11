024085606047dc9ed87b379cb6f8d574e787ccc4
Rien <rien.coertjens@gmail.com>
2018-04-11 21:44:12 +0200

    form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,28 +2,27 @@ import React from 'react';
 
 const ContactPage = props => (
   <main>
-<form name="contact" netlify-honeypot="bot-field" method="POST" action="thank-you" netlify>
-    <p class="visually-hidden"><label><input name="bot-field" /></label>
-    </p>
-    <p>
-      <label>
-        Name: <input type="text" name="name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Email: <input type="text" name="email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Message: <textarea name="message"></textarea>
-      </label>
-    </p>
-    <p>
-      <button>Send</button>
-    </p>
-  </form>    
+    <form name="contact" netlify-honeypot="bot-field" method="POST" netlify>
+      <input name="bot-field" className="visually-hidden" />
+      <p>
+        <label>
+          Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email: <input type="text" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button>Send</button>
+      </p>
+    </form>
   </main>
 );
 
