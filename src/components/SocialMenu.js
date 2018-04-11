9ba0e6ab3c65ea096e6b8bfb12383d95f7a33491
Rien <rien.coertjens@gmail.com>
2018-04-11 23:43:43 +0200

    social url updates

--- src/components/SocialMenu.js
+++ src/components/SocialMenu.js
@@ -11,8 +11,8 @@ import {
 
 const SocialMenu = props => (
   <nav className="social">
-    <a target="_blank" href="http://facebook.com/Webhartonline"><IoSocialFacebook /></a>
-    <a target="_blank" href="http://instagram.com"><IoSocialInstagram /></a>
+    <a target="_blank" href="https://www.facebook.com/webhartdevelopment/"><IoSocialFacebook /></a>
+    <a target="_blank" href="https://www.instagram.com/web_hart/"><IoSocialInstagram /></a>
     <a target="_blank" href="https://api.whatsapp.com/send?phone=32499461295"><IoSocialWhatsapp /></a>
     <Link to="/contact"><IoChatbubbles /></Link>
   </nav>
