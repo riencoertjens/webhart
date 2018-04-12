38902c879e7a883f9d95614af5b51977f320ee1a
Rien <rien.coertjens@gmail.com>
2018-04-12 17:33:39 +0200

    phone number update

--- src/components/SocialMenu.js
+++ src/components/SocialMenu.js
@@ -13,7 +13,7 @@ const SocialMenu = props => (
   <nav className="social">
     <a target="_blank" href="https://www.facebook.com/webhartdevelopment/"><IoSocialFacebook /></a>
     <a target="_blank" href="https://www.instagram.com/web_hart/"><IoSocialInstagram /></a>
-    <a target="_blank" href="https://api.whatsapp.com/send?phone=32499461295"><IoSocialWhatsapp /></a>
+    <a target="_blank" href="https://api.whatsapp.com/send?phone=34722256067"><IoSocialWhatsapp /></a>
     <Link to="/contact"><IoChatbubbles /></Link>
   </nav>
 )
