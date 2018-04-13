0248fac762d612945cbe141713680585bf94ab87
Rien <rien.coertjens@gmail.com>
2018-04-13 19:59:16 +0200

    copyright text update

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Copyright = props => (
-  <span className="copyright">&copy; WEBhart - 2018 | created with <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a> | design inspired by <a href="https://html5up.net/dimension" target="_blank">HTML5 UP - dimension</a></span>
+  <span className="copyright">Copyright &copy; WEBhart - 2018. All Rights Reserved.<br/>Created with <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a> | design inspired by <a href="https://html5up.net/dimension" target="_blank">HTML5 UP - dimension</a>.</span>
 )
 
 export default Copyright
